a7bcb6f3927cfb155587a5ab4f44c3a13782210b
franz abarquez <franzdale.abarquez@gmail.com>
2024-04-22 23:18:44 +0800

    added some more code giving users where to continue with another operation or end the application as well as tracking of the result of the previous operation

--- primitive_calc/main.cpp
+++ primitive_calc/main.cpp
@@ -29,20 +29,23 @@ float division(double *a, double *b, double *result)
 
 int main()
 {
+  int operation;
+  int continue_choice;
+  double a;
+  // B is set to 1 instead to avoid division by zero even if the division function hasn't been called yet
+  double b = 1;
+  double result;
+  double c = 1;
   bool state = true;
   while (state)
   {
-    int operation;
-    double a;
-    // B is set to 1 instead to avoid division by zero even if the division function hasn't been called yet
-    double b = 1;
-    double result;
 
     cout << "Enter the number corresponding the chosen operation to be performed. Be mindful of the order of the input variables for subtraction and division" << endl
          << "[1] Addition       | result=a+b " << endl
          << "[2] Subtraction    | result=a-b " << endl
          << "[3] Multiplication | result=a*b " << endl
-         << "[4] Division       | result=a/b " << endl;
+         << "[4] Division       | result=a/b " << endl
+         << "Result of last operation, input 'c' as variable input to reuse value: " << c << endl;
 
     cin >> operation;
     cout << "Enter the first value (a): ";
@@ -73,7 +76,32 @@ int main()
       break;
 
     default:
-      cout << "Something went wrong!";
+      cout << "Something went wrong!" << endl;
+      break;
+    }
+
+    c = result;
+
+    cout << "Variable a: " << a << endl
+         << "Variable b: " << b << endl
+         << "Variable c: " << c << endl;
+
+    cout << "Do you wish to perform another operation?" << endl
+         << "[1] Yes" << endl
+         << "[2] No " << endl;
+
+    cin >> continue_choice;
+    switch (continue_choice)
+    {
+    case 1:
+      state = true;
+      break;
+    case 2:
+      state = false;
+      break;
+    default:
+      cout << "Something went wrong. Will proceed to close the end application" << endl;
+      state = false;
       break;
     }
   }
