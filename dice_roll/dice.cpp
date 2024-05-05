06833e35d826044b0c80ff6fe8b7f2d688ba2744
franz abarquez <franzdale.abarquez@gmail.com>
2024-05-06 01:03:04 +0800

    modified the code for dice_roll

--- dice_roll/dice.cpp
+++ dice_roll/dice.cpp
@@ -1,32 +1,70 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <vector>
+#include <random>
 
 using std::cin;
 using std::cout;
 using std::endl;
 
+using std::default_random_engine;
+using std::random_device;
+
+using std::vector;
+
+vector<int> vectorOut(vector<int> a)
+{
+  int vector_size = a.size();
+  cout << "{";
+
+  for (int i = 0; i < vector_size; i++)
+  {
+    if (i != vector_size - 1)
+    {
+      cout << a[i] << ", ";
+    }
+    else
+    {
+      cout << a[i];
+    }
+  }
+  cout << "}" << endl;
+}
+
 int roll(int dice_side)
 {
-  srand(static_cast<unsigned int>(time(NULL)));
   int randomNumber = rand();
   int result = (randomNumber % dice_side) + 1;
 
   return result;
 }
 
+int roll_b(int dice_side)
+{
+  static default_random_engine generator(random_device{}());
+  std::uniform_int_distribution<int> distribution(1, dice_side);
+
+  return distribution(generator);
+}
+
 int main()
 {
+  srand(static_cast<unsigned int>(time(NULL)));
 
-  int dice_side = 6;
-  int number_of_dice = 3;
+  int dice_side = 20;
+  int number_of_dice = 5;
+  vector<int> result_array;
 
-  int roll_dice = roll(dice_side);
-
-  cout << "The result of rolling a " << dice_side << "-sided die is " << roll_dice << endl;
-
-  for (int i = 0; i < dice_side; i++)
+  for (int i = 0; i < number_of_dice; i++)
   {
+
+    int roll_result = roll(dice_side);
+    result_array.push_back(roll_result);
   }
+
+  cout << "The result of rolling " << number_of_dice << " " << dice_side << "-sided dice is as follows: ";
+  vectorOut(result_array);
+
   return 0;
 }
